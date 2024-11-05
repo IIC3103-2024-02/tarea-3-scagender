85c9ee07f960286064ac826fdf8d1d38011e88e5
Scagender <scagender@scagender>
2024-11-04 22:13:42 -0300

    FIX

--- client/src/components/Metrics/Metrics.js
+++ client/src/components/Metrics/Metrics.js
@@ -20,7 +20,6 @@ function ChatPage() {
       // Realizar la solicitud a la API
       const response = await fetch('https://tarea-3-scagender.onrender.com/api/guiones/ask-question', {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ query: newMessage }),
       });
       console.log(response);
